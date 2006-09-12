00d1af234f23dc94c47406bb8891abd843f6242f
davebenson <davebenson@5eb64970-ff13-0410-8b82-f76cb71ab3e0>
2006-09-12 15:23:31 +0000

    An idea i've been mucking around with: symbolic socket-addresses as simple way to further bury the dns routines. This could be used to simplify UrlTransferHttp and PersistentConnection,
    but we'll have to make gskstreamfd able to handle symbolic addresses first.

---
+++ src/gsksocketaddresssymbolic.h
@@ -0,0 +1,68 @@
+GType gsk_socket_address_symbolic_get_type(void) G_GNUC_CONST;
+GType gsk_socket_address_symbolic_ipv4_get_type(void) G_GNUC_CONST;
+#define GSK_TYPE_SOCKET_ADDRESS_SYMBOLIC              (gsk_socket_address_symbolic_get_type ())
+#define GSK_SOCKET_ADDRESS_SYMBOLIC(obj)              (G_TYPE_CHECK_INSTANCE_CAST ((obj), GSK_TYPE_SOCKET_ADDRESS_SYMBOLIC, GskSocketAddressSymbolic))
+#define GSK_SOCKET_ADDRESS_SYMBOLIC_CLASS(klass)      (G_TYPE_CHECK_CLASS_CAST ((klass), GSK_TYPE_SOCKET_ADDRESS_SYMBOLIC, GskSocketAddressSymbolicClass))
+#define GSK_SOCKET_ADDRESS_SYMBOLIC_GET_CLASS(obj)    (G_TYPE_INSTANCE_GET_CLASS ((obj), GSK_TYPE_SOCKET_ADDRESS_SYMBOLIC, GskSocketAddressSymbolicClass))
+#define GSK_IS_SOCKET_ADDRESS_SYMBOLIC(obj)           (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GSK_TYPE_SOCKET_ADDRESS_SYMBOLIC))
+#define GSK_IS_SOCKET_ADDRESS_SYMBOLIC_CLASS(klass)   (G_TYPE_CHECK_CLASS_TYPE ((klass), GSK_TYPE_SOCKET_ADDRESS_SYMBOLIC))
+#define GSK_TYPE_SOCKET_ADDRESS_SYMBOLIC_IPV4              (gsk_socket_address_symbolic_ipv4_get_type ())
+#define GSK_SOCKET_ADDRESS_SYMBOLIC_IPV4(obj)              (G_TYPE_CHECK_INSTANCE_CAST ((obj), GSK_TYPE_SOCKET_ADDRESS_SYMBOLIC_IPV4, GskSocketAddressSymbolicIpv4))
+#define GSK_SOCKET_ADDRESS_SYMBOLIC_IPV4_CLASS(klass)      (G_TYPE_CHECK_CLASS_CAST ((klass), GSK_TYPE_SOCKET_ADDRESS_SYMBOLIC_IPV4, GskSocketAddressSymbolicIpv4Class))
+#define GSK_SOCKET_ADDRESS_SYMBOLIC_IPV4_GET_CLASS(obj)    (G_TYPE_INSTANCE_GET_CLASS ((obj), GSK_TYPE_SOCKET_ADDRESS_SYMBOLIC_IPV4, GskSocketAddressSymbolicIpv4Class))
+#define GSK_IS_SOCKET_ADDRESS_SYMBOLIC_IPV4(obj)           (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GSK_TYPE_SOCKET_ADDRESS_SYMBOLIC_IPV4))
+#define GSK_IS_SOCKET_ADDRESS_SYMBOLIC_IPV4_CLASS(klass)   (G_TYPE_CHECK_CLASS_TYPE ((klass), GSK_TYPE_SOCKET_ADDRESS_SYMBOLIC_IPV4))
+
+/* "symbolic" addresses */
+struct void (*GskSocketAddressSymbolicResolveFunc) (GskSocketAddressSymbolic *orig,
+                                                    GskSocketAddress         *resolved,
+                                                    gpointer                  user_data);
+struct void (*GskSocketAddressSymbolicErrorFunc)   (GskSocketAddressSymbolic *orig,
+                                                    const GError             *error,
+                                                    gpointer                  user_data);
+
+struct _GskSocketAddressSymbolicClass
+{
+  GskSocketAddressClass socket_address_class;
+  gpointer (*create_name_resolver) (GskSocketAddressSymbolic *symbolic);
+  void     (*start_resolution)     (GskSocketAddressSymbolic *,
+                                    gpointer                  name_resolver,
+                                    GskSocketAddressSymbolicResolveFunc r,
+                                    GskSocketAddressSymbolicErrorFunc e,
+                                    gpointer                  user_data,
+                                    GDestroyNotify            destroy);
+  void     (*cancel_resolution)    (GskSocketAddressSymbolic *symbolic,
+                                    gpointer                  name_resolver);
+
+};
+struct _GskSocketAddressSymbolic
+{
+  GskSocketAddress      socket_address;
+  char                 *name;
+};
+struct _GskSocketAddressSymbolicIpv4Class
+{
+  GskSocketAddressSymbolicClass base_class;
+};
+struct _GskSocketAddressSymbolicIpv4
+{
+  GskSocketAddressSymbolic base_instance;
+  guint16                  port;
+};
+
+GskSocketAddress *gsk_socket_address_symbolic_ipv4_new (const char *name,
+                                                        guint16     port);
+
+
+gpointer gsk_socket_address_symbolic_create_name_resolver
+                                     (GskSocketAddressSymbolic *symbolic);
+void     gsk_socket_address_symbolic_start_resolution 
+                                     (GskSocketAddressSymbolic *symbolic,
+                                      gpointer                  name_resolver,
+                                      GskSocketAddressSymbolicResolveFunc r,
+                                      GskSocketAddressSymbolicErrorFunc e,
+                                      gpointer                  user_data,
+                                      GDestroyNotify            destroy);
+void     gsk_socket_address_symbolic_cancel_resolution
+                                     (GskSocketAddressSymbolic *symbolic,
+                                      gpointer                  name_resolver);
